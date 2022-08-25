bb13da3cfb5fb9a39d8d330a02ea829d1f87121f
Mert <107802530+MertOzkan35@users.noreply.github.com>
2022-08-25 18:57:20 +0300

    App js created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import "./App.css";
-import Header from "./Components/Header";
+
+import Context from "./Components/Context";
 
 function App() {
   return (
-    <div className="w-full h-[768px] bg-[#191f25]">
-      <Header />
+    <div>
+      <Context />
     </div>
   );
 }
 
 export default App;
+
+{
+  /* <div className="w-full h-[868px] bg-[#191f25] flex flex-col  items-center">
+      <Header />
+      <div className="w-3/6 h-[600px] flex flex-col-2  bg-[#ebeef0]">
+        <div className="w-3/5 h-full  ">
+          <div className="w-full h-2/5  border border-black p-6">
+            <Balance />
+          </div>
+          <div className="w-full h-3/5 p-6 border border-black bg-[#ebeef0]">
+            <NewTransaction />
+          </div>
+        </div>
+        <div className="w-2/5 h-full border p-6 border-black bg-[#ebeef0]">
+          <TransactionHistory />
+       </div>
+      </div>
+    </div> */
+}
